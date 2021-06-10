0ed992cf221de797f3e39c88cd09182c48c84231
kataplan <dcatalanmarchese@gmail.com>
2021-06-10 15:32:35 -0400

    comentario

--- src/app/pages/product/coment/coment.component.ts
+++ src/app/pages/product/coment/coment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-coment',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComentComponent implements OnInit {
 
-  constructor() { }
+  constructor() 
+  {this.formulario =this.createFormGroup();}
+  createFormGroup(){
+    return new FormGroup({
+      rate:new FormControl('',[Validators.required]),
+      coment:new FormControl('',[Validators.required, Validators.minLength(5)]),
+     
+    });
+  }
+  formulario:FormGroup;
 
   ngOnInit(): void {
   }
 
-}
+  sendComent(){
+    if(!this.formulario.valid){
+      alert("Complete el Formulario");
+      return;
+    }
+    if(this.formulario.value.genero==""){
+      alert("Debe elegir valoración");
+      return;
+    }
+    
+  }
+  }
+
+
